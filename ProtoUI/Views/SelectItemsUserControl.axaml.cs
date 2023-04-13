dc9d75d885811679202bf5e3e39935b69c679e3e
Viktor <kassov@ukr.net>
2023-04-13 11:38:13 +0300

    SelectItems

---
+++ ProtoUI/Views/SelectItemsUserControl.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ProtoUI.Views
+{
+    public partial class SelectItemsUserControl : UserControl
+    {
+        public SelectItemsUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
