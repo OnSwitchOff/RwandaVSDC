432a68a3c3e2bc274adc81832f3c8b0af299d5e7
Viktor <kassov@ukr.net>
2023-04-13 11:43:44 +0300

    SelectImportItems

---
+++ ProtoUI/Views/SelectImportItemsUserControl.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ProtoUI.Views
+{
+    public partial class SelectImportItemsUserControl : UserControl
+    {
+        public SelectImportItemsUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
