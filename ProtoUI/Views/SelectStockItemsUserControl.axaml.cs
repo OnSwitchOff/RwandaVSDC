1becd38bd07242311d370d42c550e61b9795ee8d
Viktor <kassov@ukr.net>
2023-04-18 14:38:37 +0300

    SelectStockItems

---
+++ ProtoUI/Views/SelectStockItemsUserControl.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ProtoUI.Views
+{
+    public partial class SelectStockItemsUserControl : UserControl
+    {
+        public SelectStockItemsUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
